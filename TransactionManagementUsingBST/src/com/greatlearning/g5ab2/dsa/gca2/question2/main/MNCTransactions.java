f8648079d8e621e71d08c4c25cd8219b7f7440f8
Jagdev Singh Dosanjh <33898246+jagdevsinghdosanjh@users.noreply.github.com>
2022-11-14 01:51:25 +0530

    Commiting to upload MNCTransaction files for q2
    Commiting to Upload files for Questin 2

---
+++ TransactionManagementUsingBST/src/com/greatlearning/g5ab2/dsa/gca2/question2/main/MNCTransactions.java
@@ -0,0 +1,49 @@
+package com.greatlearning.g5ab2.dsa.gca2.question2.main;
+
+import com.greatlearning.g5ab2.dsa.gca2.question2.model.*;
+class MNCTransactions {
+	Node node;
+	Node prev = null;
+	Node newHead = null;
+
+	// Method to Transform Binary Search Tree to skewed tree
+	void processBST(Node root) {
+		if (root == null)
+			return;
+		processBST(root.left);
+		Node rightNode = root.right;
+
+		if (newHead == null) {
+			newHead = root;
+			root.left = null;
+			prev = root;
+		} else {
+			prev.right = root;
+			root.left = null;
+			prev = root;
+		}
+		processBST(rightNode);
+	}
+
+	// Method to Print the Transformed Data
+	void traverse(Node root) {
+		if (root == null)
+			return;
+		System.out.print(root.val + " ");
+		traverse(root.right);
+	}
+
+	public static void main(String[] args) {
+
+		MNCTransactions tree = new MNCTransactions();
+		//Node uc=new Node();
+		//UtilityClass uc=new UtilityClass();
+		tree.node = new Node(50);
+		tree.node.left = new Node(30);
+		tree.node.right = new Node(60);
+		tree.node.left.left = new Node(10);
+		tree.node.right.left = new Node(55);
+		tree.processBST(tree.node);
+		tree.traverse(tree.newHead);
+	}
+}
---
+++ TransactionManagementUsingBST/src/com/greatlearning/g5ab2/dsa/gca2/question2/model/Node.java
@@ -0,0 +1,12 @@
+package com.greatlearning.g5ab2.dsa.gca2.question2.model;
+
+public class Node {
+	public int val;
+	public Node left;
+	public Node right;
+
+	public Node(int val) {
+		this.val = val;
+		left = right = null;
+	}
+}
